f5a5bb2998fa126706baa2d41ade8c89fa4df576
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2023-06-28 20:38:05 -0300

    Adding all of the 4 CRUD mocked operations to the project

--- src/main/java/br/com/erudio/services/PersonServices.java
+++ src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,5 +1,8 @@
 package br.com.erudio.services;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,7 +16,17 @@ public class PersonServices {
 	private final AtomicLong counter =  new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
-	public Person findById(String id) {	
+	public List<Person> findAll() {	
+		logger.info("Finding all people!");
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons ;
+	}
+
+	public Person findById(String id) {		
 		logger.info("Finding one person!");
 		
 		Person person = new Person();
@@ -22,6 +35,31 @@ public class PersonServices {
 		person.setLastName("Vieira");
 		person.setAddress("Palhoça");
 		person.setGender("Male");
+		
+		return person;
+	}
+	
+	public Person create(Person person) {
+		logger.info("Creating one person!");
+		return person;
+	}
+	
+	public Person update(Person person) {
+		logger.info("Updating one person!");
+		return person;
+	}
+	
+	public void delete(String id) {
+		logger.info("Deleting one person!");
+	}
+	
+	private Person mockPerson(int i) {	
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brasil " + i);
+		person.setGender("Gender " + i);
 		
 		return person;
 	}
